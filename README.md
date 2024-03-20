ea083c2eed2ad8a7fa27641aac370b86c6f3779b
Justin <jniestroy@gmail.com>
2024-03-20 13:55:06 -0400

    csv details more pydantic

--- README.md
+++ README.md
@@ -1,4 +1,9 @@
 # RedCap Import Library
 
-Pseudo-Code for future development:
-    
+RedCap Data Dictionary CSV Elements
+<br><br>Need:
+1. Field Name -> Name
+2. Field Label -> Description
+3. Field Type -> Data Type
+4. Choices, Calculations, OR Slider Labels -> Options, will need logic to parse
+5. Required Field? -> Boolean required 
--- RedCapSchema.py
+++ RedCapSchema.py
@@ -1,6 +1,10 @@
-from typing import Optional
+from typing import Optional, List
 from pydantic import BaseModel, Field
 
+class DataDictionaryItem(BaseModel):
+    field_name: str
+    field_type: str
+    description: str
 
 class RedCapSchema(BaseModel):
     metadataType: str = Field(
@@ -10,11 +14,15 @@ class RedCapSchema(BaseModel):
     )
     name: str = Field(max_length=200)
     description: str = Field(min_length=5)
-    file_path: str = Field(..., title="File Path", description="Path to the file")
+    properties: Optional[List[DataDictionaryItem]] = []
+
+    def parse_schema_csv(self, file_path: str) -> None:
+        """Function to load schema file"""
+        pass
 
 # Example usage
 redcap_schema = RedCapSchema(
     name="Example Schema",
-    description="Sample Schema for PreMO RedCapDB.",
-    file_path="/path/to/file"
+    description="Sample Schema for PreMO RedCapDB."
 )
+print(redcap_schema.model_dump_json(by_alias=True, indent=2))
