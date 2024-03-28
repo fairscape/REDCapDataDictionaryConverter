b069aeb7bee4fc46b9defe2d0b2a13a2f3280ba5
Justin <jniestroy@gmail.com>
2024-03-28 11:44:21 -0400

    split parse into methods

--- RedCapSchema.py
+++ RedCapSchema.py
@@ -9,10 +9,10 @@ default_context = {
     "@vocab": "https://schema.org/",
     "evi": "https://w3id.org/EVI#"
 }
-class DataDictionaryItem(BaseModel):
-    """Class that holds required information for a redcap variable"""
-    field_name: str
-    field_type: str
+
+class DataProperty(BaseModel):
+    """Represents a single variable in a RedCap data dictionary."""
+    type: str
     description: str
     index: int
     pattern: Optional[str] = None
@@ -20,86 +20,89 @@ class DataDictionaryItem(BaseModel):
     maximum: Optional[float] = None
 
 class RedCapSchema(BaseModel):
-    """Class for loading and converting redcap schema to json-ld"""
-    context: Dict[str, str] = Field(
-        default=default_context,
-        title="context",
-        alias="@context"
-    )
-    metadataType: str = Field(
-        title="metadataType",
-        alias="@type",
-        default='EVI:Schema'
-    )
+    """Represents a schema for a RedCap database, which can be converted to JSON-LD."""
+    context: Dict[str, str] = Field(default=default_context, title="context", alias="@context")
+    metadataType: str = Field(title="metadataType", alias="@type", default='EVI:Schema')
     name: str = Field(max_length=200)
     description: str = Field(min_length=5)
-    properties: Optional[List[DataDictionaryItem]] = []
+    properties: Optional[Dict[str, DataProperty]] = {}
     type: Optional[str] = Field(default="object")
     additionalProperties: Optional[bool] = Field(default=True)
-    required: Optional[List] = []
+    required: Optional[List[str]] = []  
     separator: Optional[str] = Field(default=",")
     header: Optional[bool] = Field(default=True)
-    examples: Optional[List] = []
+    examples: Optional[List[Dict]] = []  
 
     @staticmethod
     def generate_regex_from_string(input_string: str) -> str:
-        """Takes redcap rules for radio display and converts to allowed regex"""
-        # Split the string into options
+        """Converts RedCap radio display rules to a regex pattern."""
         options = [option.strip() for option in input_string.split("|")]
-
-        # Extract the text portion of each option and escape special characters
         text_options = [re.escape(option.split(",")[1].strip()) for option in options]
-
-        # Join the options with the pipe operator for the regex pattern
         pattern = f"^({'|'.join(text_options)})$"
-
         return pattern
 
     def parse_schema_csv(self, file_path: str, use_numeric_values: bool = False) -> None:
-        """Function to parse input csv into properties"""
-        df = pd.read_csv(file_path)
-        properties = []
+        """Parses a RedCap data dictionary CSV file to populate the schema properties."""
+        try:
+            df = pd.read_csv(file_path)
+        except FileNotFoundError:
+            raise Exception(f"File {file_path} not found.")
+        except pd.errors.ParserError:
+            raise Exception(f"Error parsing CSV file {file_path}.")
+
+        #For every row in the df create a key:value pair describing that property
+        self.properties = {
+            row['Variable / Field Name']: self._create_data_dictionary_item(row, index, use_numeric_values)
+            for index, row in df.iterrows()
+        }
+
+    def _create_data_dictionary_item(self, row: pd.Series, index: int, use_numeric_values: bool) -> DataProperty:
+        """Creates a DataProperty from a row of the CSV file."""
+        var = row['Variable / Field Name']
+        form_name = row['Form Name']
+        description = row['Field Label']
+        # Check if description is missing
+        if pd.isna(description) or description.strip() == '':
+            description = var + " " + form_name
+        var_type = row['Field Type']
+        type_details = row['Text Validation Type OR Show Slider Number']
+        choices = row['Choices, Calculations, OR Slider Labels']
+
+        field_type, pattern = self._determine_field_type_and_pattern(
+            var_type, type_details, choices, use_numeric_values
+        )
+
+        minimum = row['Text Validation Min'] if not np.isnan(row['Text Validation Min']) else None
+        maximum = row['Text Validation Max'] if not np.isnan(row['Text Validation Max']) else None
+
+        return DataProperty(
+            type=field_type,
+            description=description,
+            index=index,
+            pattern=pattern,
+            minimum=minimum,
+            maximum=maximum
+        )
+
+    def _determine_field_type_and_pattern(self, var_type: str, type_details: str, choices: str, use_numeric_values: bool) -> (str, Optional[str]):
+        """Determines the field type and pattern for a variable based on its RedCap type and details."""
         pattern = None
-        
-        for index, row in df.iterrows():
-            var = row['Variable / Field Name']
-            form_name = row['Form Name']
-            description = row['Field Label']
-            if pd.isna(description) or description.strip() == '':
-                description = var + " " + form_name
-            var_type = row['Field Type']
-            type_details = row['Text Validation Type OR Show Slider Number']
-            choices = row['Choices, Calculations, OR Slider Labels']
 
-            if var_type == 'yesno':
-                field_type = 'boolean'
-            elif type_details == 'integer':
-                field_type = 'integer'
-            elif type_details == 'number':
-                field_type = 'number'
-            elif var_type in ['radio', 'dropdown']:
-                if use_numeric_values:
-                    field_type = 'integer'
-                else:
-                    pattern = self.generate_regex_from_string(choices)
-                    field_type = 'string'
-            else:
-                field_type = 'string'
+        if var_type == 'yesno':
+            field_type = 'boolean'
+        elif type_details == 'integer':
+            field_type = 'integer'
+        elif type_details == 'number':
+            field_type = 'number'
+        elif var_type in ['radio', 'dropdown']:
+            field_type = 'integer' if use_numeric_values else 'string'
+            if not use_numeric_values:
+                pattern = self.generate_regex_from_string(choices)
+        else:
+            field_type = 'string'
 
-            minimum = row['Text Validation Min'] if not np.isnan(row['Text Validation Min']) else None
-            maximum = row['Text Validation Max'] if not np.isnan(row['Text Validation Max']) else None
+        return field_type, pattern
 
-            properties.append(DataDictionaryItem(
-                field_name=var,
-                field_type=field_type,
-                description=description,
-                index=index,
-                pattern=pattern,
-                minimum=minimum,
-                maximum=maximum
-            ))
-
-        self.properties = properties
 
     def save_to_json(self, output_file: str) -> None:
         """Function to save the schema to a JSON file"""
